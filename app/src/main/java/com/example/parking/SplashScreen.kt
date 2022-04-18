baa3316075546238dad26176930e82c0b86acd0c
Khoroshavina Ekaterina <63115848+KatrinLion14@users.noreply.github.com>
2022-04-18 21:29:58 +0300

    Added user screen, menu for admin, login with email, expanded view; fixed icons; temporary class reservation

--- app/src/main/java/com/example/parking/SplashScreen.kt
+++ app/src/main/java/com/example/parking/SplashScreen.kt
@@ -9,6 +9,7 @@ import android.content.Intent
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_splash_screen)
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
         finish()
