e628bd2c021fd7a376de83360f23b9f8bd64a7f9
Andrey <bakurskii2001@gmail.com>
2022-05-27 01:08:07 +0300

    prepair for implement ELM to CarsFragment

--- app/src/main/java/com/example/parking/presentation/fragments/carsFragment/CarsFragment.kt
+++ app/src/main/java/com/example/parking/presentation/fragments/carsFragment/CarsFragment.kt
@@ -7,17 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AbsListView
 import android.widget.ExpandableListView
+import android.widget.FrameLayout
 import androidx.fragment.app.Fragment
 import com.example.parking.presentation.activities.AdminActivity.AdminMainActivity
 import com.example.parking.presentation.activities.CreateModelActivity.CreateModelActivity
 import com.example.parking.R
 import com.example.parking.presentation.adapters.ExpListAdapterAdminCars
 import com.example.parking.data.models.Car
+import com.example.parking.presentation.fragments.carsFragment.elm.Effect
+import com.example.parking.presentation.fragments.carsFragment.elm.Event
+import com.example.parking.presentation.fragments.carsFragment.elm.State
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import vivid.money.elmslie.android.base.ElmFragment
 import java.util.*
 import kotlin.collections.ArrayList
 
-class CarsFragment : Fragment() {
+class CarsFragment : ElmFragment<Event, Effect, State>() {
+
+    private var progressBar : FrameLayout? = null
+    private var carsInAdapter : ArrayList<Car>? = null
+    private var carsAdapter: ExpListAdapterAdminCars? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,23 +35,21 @@ class CarsFragment : Fragment() {
         val rootView: View = inflater.inflate(R.layout.fragment_cars, null)
         val listView = rootView.findViewById<ExpandableListView>(R.id.expListView)
 
-        val groups = ArrayList<Car>()
+        progressBar = rootView.findViewById<FrameLayout>(R.id.progressBarContainer)
 
-        val car1 = Car(
-            id = UUID.fromString("0f29717c-37e4-4a93-9165-6baacae64e98"),
-            model = "Nissan",
-            length = 3000,
-            wight = 1800,
-            registryNumber = "E475CX152"
-        )
-        groups.add(car1)
+//        val groups = ArrayList<Car>()
 
-        val adapter =
-            ExpListAdapterAdminCars(
-                activity,
-                groups
-            )
-        listView.setAdapter(adapter)
+//        val car1 = Car(
+//            id = UUID.fromString("0f29717c-37e4-4a93-9165-6baacae64e98"),
+//            model = "Nissan",
+//            length = 3000,
+//            wight = 1800,
+//            registryNumber = "E475CX152"
+//        )
+
+        carsAdapter = ExpListAdapterAdminCars(activity, carsInAdapter)
+        listView.setAdapter(carsAdapter)
+
 
         val btAdd = rootView.findViewById<FloatingActionButton>(R.id.fab)
         btAdd.setOnClickListener {
@@ -55,6 +62,7 @@ class CarsFragment : Fragment() {
             intent.putExtra("fragment", data)
             (activity as AdminMainActivity).startActivity(intent)
         }
+
         listView.setOnScrollListener(object : AbsListView.OnScrollListener {
             override fun onScrollStateChanged(view: AbsListView?, scrollState: Int) {}
             override fun onScroll(
@@ -72,4 +80,24 @@ class CarsFragment : Fragment() {
 
         return rootView
     }
+
+    override val initEvent: Event = Event.Ui.LoadCars
+
+    override fun render(state: State) {
+        if (state.loading) {
+            progressBar!!.visibility = View.VISIBLE
+        } else {
+            progressBar!!.visibility = View.INVISIBLE
+        }
+
+//        if (state.doUpdate) {
+//            carsInAdapter!!.clear()
+//            carsInAdapter = carsInAdapter + state.cars
+//        }
+    }
+
+    override fun handleEffect(effect: Effect): Unit? {
+        return super.handleEffect(effect)
+    }
+
 }
