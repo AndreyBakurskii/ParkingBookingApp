959c9d704458950ccda502c56293a6476c3e63b5
Andrey <bakurskii2001@gmail.com>
2022-06-06 15:25:19 +0300

    prepair Employee data

--- app/src/main/java/com/example/parking/data/network/api/EmployeeAPI.kt
+++ app/src/main/java/com/example/parking/data/network/api/EmployeeAPI.kt
@@ -1,8 +1,10 @@
 package com.example.parking.data.network.api
 
 import com.example.parking.data.network.modelJSON.EmployeeJson
+import io.reactivex.Single
 import okhttp3.ResponseBody
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.http.*
 
 
@@ -11,22 +13,22 @@ const val EMPLOYEES_DETAIL_URL: String = "/employees/{id}"
 
 interface EmployeeAPI {
     @GET(EMPLOYEES_URL)
-    fun getEmployees(): Call<MutableList<EmployeeJson>>
+    fun getEmployees(): Single<Response<List<EmployeeJson>>>
 
     @POST(EMPLOYEES_URL)
-    fun createEmployee(@Body employee: HashMap<String, Any>): Call<EmployeeJson>
+    fun createEmployee(@Body employee: HashMap<String, String>): Single<Response<EmployeeJson>>
 
     // todo: возращается просто статус
     @DELETE(EMPLOYEES_URL)
-    fun deleteEmployees(): Call<ResponseBody>
+    fun deleteEmployees(): Single<Response<ResponseBody>>
 
     @GET(EMPLOYEES_DETAIL_URL)
-    fun getEmployeeDetail(@Path("id") id: String): Call<EmployeeJson>
+    fun getEmployeeDetail(@Path("id") id: String): Single<Response<EmployeeJson>>
 
     @PUT(EMPLOYEES_DETAIL_URL)
-    fun updateEmployeeDetail(@Path("id") id: String, @Body employee: HashMap<String, Any>): Call<EmployeeJson>
+    fun updateEmployeeDetail(@Path("id") id: String, @Body employee: HashMap<String, String>): Single<Response<EmployeeJson>>
 
     // todo: возращается просто статус
     @DELETE(EMPLOYEES_DETAIL_URL)
-    fun deleteEmployeeDetail(@Path("id") id: String): Call<ResponseBody>
+    fun deleteEmployeeDetail(@Path("id") id: String): Single<Response<ResponseBody>>
 }
