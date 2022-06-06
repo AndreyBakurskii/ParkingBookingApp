0e2482703751b275e888e4c54aa7563dd188fb8b
Andrey <bakurskii2001@gmail.com>
2022-06-07 01:00:29 +0300

    refactore some ELM code

--- app/src/main/java/com/example/parking/presentation/fragments/car/list/elm/Store.kt
+++ app/src/main/java/com/example/parking/presentation/fragments/car/list/elm/Store.kt
@@ -15,9 +15,9 @@ import vivid.money.elmslie.core.store.dsl_reducer.DslReducer
 import vivid.money.elmslie.core.store.dsl_reducer.ScreenDslReducer
 
 
-class Reducer : DslReducer<Event, State, Effect, Command>() {
+class Reducer : ScreenDslReducer<Event, Ui, Internal, State, Effect, Command>(Ui::class, Internal::class) {
 
-    override fun Result.reduce(event: Event) = when (event) {
+    override fun Result.internal(event: Internal) = when (event) {
         is Internal.SuccessLoadCars -> {
             state { copy(
                 loading = false,
@@ -44,7 +44,9 @@ class Reducer : DslReducer<Event, State, Effect, Command>() {
             state { copy(loading = false, doUpdate = false) }
             effects { +Effect.ShowErrorNetwork }
         }
+    }
 
+    override fun Result.ui(event: Ui) = when (event) {
         is Ui.Init -> {
         }
         is Ui.LoadCars -> {
