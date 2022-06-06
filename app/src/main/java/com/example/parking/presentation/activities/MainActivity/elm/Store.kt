101c0f22cc7826802b8eb5e6553eef84a0f50411
Andrey <bakurskii2001@gmail.com>
2022-06-06 12:44:45 +0300

    some changes

--- app/src/main/java/com/example/parking/presentation/activities/MainActivity/elm/Store.kt
+++ app/src/main/java/com/example/parking/presentation/activities/MainActivity/elm/Store.kt
@@ -5,12 +5,12 @@ import com.example.parking.presentation.activities.MainActivity.elm.Event.Intern
 import com.example.parking.presentation.activities.MainActivity.elm.Event.Ui
 import vivid.money.elmslie.core.store.ElmStore
 import vivid.money.elmslie.core.store.NoOpActor
+import vivid.money.elmslie.core.store.dsl_reducer.DslReducer
 
 
-class Reducer : ScreenDslReducer<Event, Ui, Internal, State, Effect, Command>(
-    Ui::class, Internal::class) {
+class Reducer : DslReducer<Event, State, Effect, Command>() {
 
-    override fun Result.internal(event: Internal) = when (event) {
+    override fun Result.reduce(event: Event) = when (event) {
         is Internal.SuccessCheckAdminPassword -> {
             state { copy(pass = true) }
             effects { +Effect.ToAdminMainActivity }
@@ -27,9 +27,7 @@ class Reducer : ScreenDslReducer<Event, Ui, Internal, State, Effect, Command>(
             state { copy(pass = false) }
             effects { +Effect.ShowErrorInvalidEmail }
         }
-    }
 
-    override fun Result.ui(event: Ui) = when (event) {
         is Ui.Init -> { }
         is Ui.StartClick -> {
             state { copy(pass = false) }
